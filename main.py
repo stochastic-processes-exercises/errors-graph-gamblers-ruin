7f7c5c0845e9bd74b281a9db461cdea89e36d267
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-21 17:08:14 +0100

    First go at answers

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import scipy.stats
 import numpy as np
 
 # Your code to simulate the walker and to calculate the errors goes here
+def random_walker(start, n, p) :
+  while (start>0 and start<n ) :
+     if np.random.uniform(0,1)<p : start=start+1
+     else : start=start-1
+  if start==0 : return 1
+  else : return 0
 
+def sample_mean(start,n,p,m) :
+  S, S2 = 0, 0
+  for i in range(m) :
+      myvar = random_walker( start, n, p )
+      S, S2 = S + myvar, S + myvar*myvar
+  mean = S / m
+  var = (m/(m-1))*( S2/m - mean*mean )
+  eee = np.sqrt(var/m)*scipy.stats.norm.ppf(0.95)
+  return mean, eee
+
+x, y = np.linspace(1,9,9), np.zeros(9)
+for i in range(1,10) : x[i-1], y[i-1], error[i-1] = i*0.1, sample_mean(5,10,i*0.1,200)
 
 plt.errorbar( x, y, yerr=error, fmt='ko' )
 plt.xlabel("Probability of winning each game")
